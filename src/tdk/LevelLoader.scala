65ce95ac7d584016b485b58c7ce3d62d800da75f
Anton Pirhonen <anton.pirhonen@aalto.fi>
2019-04-27 22:17:33 +0300

    Level-loading with headers
    Started working on level-loading with headers to identify different
    sections of the .level-file. loading monsters should be ok.

--- src/tdk/LevelLoader.scala
+++ src/tdk/LevelLoader.scala
@@ -13,39 +13,55 @@ class Wave(enemies: Buffer[Monster], spawnFreq: Double)
 
 object LevelLoader {
   
-  def wakeup = Unit
+  var readingMonData  = false
+  var readingStatData = false
+  
   
   def loadGame(str: String) = {
     val gameFile = Source.fromFile(str)
     def readLine(str: String): Unit = {
+      if (readingMonData) return readMonsterData(str)
+      if (readingStatData) return Unit
+      if (str.trim.toLowerCase == "#waves") {readingMonData = true; readingStatData = false}
+      else if (str.trim.toLowerCase == "#stats") {readingMonData = false; readingStatData = true}
+      else throw new CorruptedFileException("Headers of the .level file are wrong.")
       
       
       
+      def readMonsterData(str: String): Unit = {
+        val allowed = (48 until 58).map(_.toChar).:+('.')
+        val monsAndFreq = str.trim.split(':').map(_.trim())
+        val mons = monsAndFreq.head.split(',')
+        if (mons.head.trim == "") return ()
+        val freq = if (monsAndFreq.size == 1) 0.2 
+                   else if (monsAndFreq.last.forall(char => allowed.contains(char))
+                            && monsAndFreq.last.filter(char => char == '.').size <= 1) monsAndFreq.last.toDouble
+                   else throw new CorruptedFileException("Monster spawn frequency should be entered as follows:\n"+ 
+                                                         "<data from monsters>:frequency")
+        
+        val toAdd = mons.map(mon => {
+          val monType = mon.trim.takeWhile(_ != '*')
+          val monAmount = mon.trim.reverse.takeWhile(_ != '*').reverse.toInt
+          monType match {
+            case "normal" => for (rep <- 0 until monAmount) yield new NormalMonster(-25, -25, -25*rep)
+            case "fast"   => for (rep <- 0 until monAmount) yield new FastMonster(-25, -25, -25*rep)
+            case "tank"   => for (rep <- 0 until monAmount) yield new TankMonster(-25, -25, -25*rep)
+            case _        => throw new CorruptedFileException("Monsters are not formatted correctly in the file\n"
+                                                            + "they should be written <type>*<amount> e.g. fast*10")
+          }
+        })
+        World.waves.+=(toAdd.flatten.toBuffer)
+        new Wave(toAdd.flatten.toBuffer,freq)
+        println(freq)
+        Unit
+      }
       
-      val allowed = (48 until 58).map(_.toChar).:+('.')
-      val monsAndFreq = str.trim.split(':').map(_.trim())
-      val mons = monsAndFreq.head.split(',')
-      if (mons.head.trim == "") return ()
-      val freq = if (monsAndFreq.size == 1) 0.2 
-                 else if (monsAndFreq.last.forall(char => allowed.contains(char))
-                          && monsAndFreq.last.filter(char => char == '.').size <= 1) monsAndFreq.last.toDouble
-                 else throw new CorruptedFileException("Monster spawn frequency entered wrong.")
+      def readStatData(str: String): Unit = {
+        
+      }
       
-      val toAdd = mons.map(mon => {
-        val monType = mon.trim.takeWhile(_ != '*')
-        val monAmount = mon.trim.reverse.takeWhile(_ != '*').reverse.toInt
-        monType match {
-          case "normal" => for (rep <- 0 until monAmount) yield new NormalMonster(-25, -25, -25*rep)
-          case "fast"   => for (rep <- 0 until monAmount) yield new FastMonster(-25, -25, -25*rep)
-          case "tank"   => for (rep <- 0 until monAmount) yield new TankMonster(-25, -25, -25*rep)
-          case _        => throw new CorruptedFileException("Monsters are not formatted correctly in the file\n"
-                                                          + "they should be written <type>*<amount> e.g. fast*10")
-        }
-      })
-      World.waves.+=(toAdd.flatten.toBuffer)
-      new Wave(toAdd.flatten.toBuffer,freq)
-      println(freq)
-      Unit
+      
+      
     }
     
     try {
